787e7853f229d36256e396b5c2142bd567a2156d
Jose Rios <jriosayala@deloitte.com>
2024-08-16 11:19:17 -0600

    feat(books): Add class book to compare, create and delete books

--- Program/Practice1.cs
+++ Program/Practice1.cs
@@ -3,7 +3,7 @@ using System.Runtime.Intrinsics.X86;
 
 namespace Program
 {
-    class PrimitiveDataTypesAndExpressions
+    internal class BmiCalculator
     {
         static void Main(string[] args)
         {
@@ -17,14 +17,12 @@ namespace Program
             Console.Write("Enter your height in meters: ");
             height = Convert.ToDouble(Console.ReadLine());
 
-            // Calculate BMI
             double bmi = weight / (height * height);
-            Console.WriteLine($"Your Body Mass Index (BMI) is: {bmi}");
-
-            // Display BMI category
             string category = DetermineBMICategory(bmi);
             Console.WriteLine($"You are considered: {category}");
+
         }
+
 
         private static string DetermineBMICategory(double bmi)
         {
@@ -44,6 +42,62 @@ namespace Program
             {
                 return "Obesity";
             }
+        }
+    }
+    class Book(string title, string author, string isbn)
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("\n Book list management");
+
+            List<Book> bookList = new List<Book>();
+
+            // Append books to list
+            bookList.Add(new Book("Thinking, Fast and Slow", "Daniel Kahneman", "978-0374533557"));
+            bookList.Add(new Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "978-0062316097"));
+            bookList.Add(new Book("The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses", "Eric Ries", "978-0307887894"));
+
+            // Displaying books
+            Console.WriteLine("Library:");
+            foreach (var book in bookList)
+            {
+                book.DisplayBookDetails();
+            }
+
+            // Remove a book from the list
+            Console.WriteLine("\nRemoving 'Thinking, Fast and Slow' book");
+            bookList.RemoveAll(book => book.Title == "Thinking, Fast and Slow");
+
+            // Showing after removal
+            Console.WriteLine("After removing book, library contains the following");
+            foreach (var book in bookList)
+            {
+                book.DisplayBookDetails();
+            }
+            // Compare two books
+            Console.WriteLine("\nComparing two books");
+            Book bookOne = new("Book Example One", "Academy .Net", "1234567890");
+            Book bookTwo = new("Book Example Two", "Academy Java", "0987654321");
+            if (bookOne.HasSameISBN(bookTwo))
+            {
+                Console.WriteLine("Book one and book two have the same ISBN");
+            }
+            else
+            {
+                Console.WriteLine("Book one and book two have different ISBN");
+            }
+        }
+        public string Title { get; set; } = title;
+        public string Author { get; set; } = author;
+        public string ISBN { get; set; } = isbn;
+
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, ISBN: {ISBN}");
+        }
+        public bool HasSameISBN(Book bookToCompare)
+        {
+            return ISBN == bookToCompare.ISBN;
         }
     }
 }
